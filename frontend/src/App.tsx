dadae7c9399c9ae500a3baba9640b0f5b125dfa5
Ilya Petrukhin <petrukhinilya61@list.ru>
2022-05-25 15:15:52 +0300

    Initial Landing

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BoardComponent from './components/BoardComponent'
+import './App.css';
+
+function App() {
+  return (
+    <div className='app' >
+      Amazing Chess
+      <BoardComponent/>
+    </div>
+  );
+}
+
+export default App;
